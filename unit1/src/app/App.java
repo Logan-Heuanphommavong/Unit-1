310150e18adfc9540b8928b08dc792650a9fd61e
Logan-Heuanphommavong <814850@mystma.org>
2020-04-09 15:09:58 +0000

    added top five movies

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -2,6 +2,10 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Taco Bell is very good");
+        System.out.println("1.Joker ");
+        System.out.println("2. Shutter Island");
+        System.out.println("3. Spider-Man 2");
+        System.out.println("4. Avengers:Endgame");
+        System.out.println("It");
     }
 }
