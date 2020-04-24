29055e258ba6d24e7579fa3ca3c6718a17f5c451
Logan-Heuanphommavong <814850@mystma.org>
2020-04-24 17:47:02 +0000

    quiz 1

--- unit1/src/app/App.java
+++ unit1/src/app/App.java
@@ -3,24 +3,26 @@ package app;
 public class App {
     public static void main(String[] args) throws Exception {
 
-       for (int i = 22; i <= 28; i = i + 3) 
-       {
-           System.out.println(i);
-       }
+       System.out.println("Start first loop");
 
-       for (int i = 2; i <= 32; i = i * 2){
+       for (int i = 1; i < 5; i++) {
            System.out.println(i);
-       }
-       for (int i = 3; i >= -81; i = i * -i){
-           System.out.println(i);
-       }
-       for (int i = 50; i >= 5; i = i - 5){
-           System.out.println(i);
-       }
-       for (int i = 1; i <= 9; i = i + 1){
-           System.out.println(i * i);
-           System.out.println(i + 3);
-           System.out.println(i);
-       }
+            }
+            System.out.println("Five");
+
+            for ( int i = 6; i < 11; i++) {
+                System.out.println(i);
+
+            }
+            System.out.println("End first loop");
+            System.out.println("start second loop"); {
+
+            }
+            for (int i = 100; i < 151; i + 10) {
+                System.out.println(i);
+            }
+
+
+
     }
 }
